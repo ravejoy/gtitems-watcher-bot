bf5e7c2e397697b31b473243f685ea6fd286c168
Nazarii Tsyhaniuk <nazariyt@gmail.com>
2025-09-14 17:23:36 +0100

    chore: add TypeScript config, ESLint v9 + @typescript-eslint v8, Prettier setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,35 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     project: false,
   },
-  plugins: ["@typescript-eslint", "import"],
+  plugins: ['@typescript-eslint', 'import'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
   ],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: true,
       node: true,
     },
   },
   rules: {
-    "import/order": [
-      "warn",
+    'import/order': [
+      'warn',
       {
-        "newlines-between": "always",
-        alphabetize: { order: "asc", caseInsensitive: true },
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   },
-  ignorePatterns: ["dist/", "node_modules/"],
+  ignorePatterns: ['dist/', 'node_modules/'],
 };
